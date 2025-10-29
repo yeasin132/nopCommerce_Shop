6797127d1dbd8c3678f24f5856e4ce9c5643ebc6
Yeasin Arafat <yeasinarafat24168@gmail.com>
2025-10-29 18:45:56 +0600

    Add full project updates including today_update.txt and modified files

---
+++ source/repos/nopCommerce_Shop/src/Plugins/Nop.Plugin.Misc.BasicPlugin/BasicPlugin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Services.Plugins;
+
+namespace Nop.Plugin.Misc.BasicPlugin
+{
+    public class BasicPlugin : BasePlugin
+    {
+        public BasicPlugin() { }
+
+        public override async Task InstallAsync()
+        {
+            await base.InstallAsync();
+        }
+
+        public override async Task UninstallAsync()
+        {
+            await base.UninstallAsync();
+        }
+    }
+}
---
+++ source/repos/nopCommerce_Shop/src/Plugins/Nop.Plugin.Nija.PlugSimple/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Nop.Plugin.Nija.PlugSimple")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Nop.Plugin.Nija.PlugSimple")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6ce7382c-e73c-4c6e-a9a6-b6be0c4ba770")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
